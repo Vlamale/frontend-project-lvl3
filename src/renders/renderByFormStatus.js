7675c9e0d0043031457c8461fbe8f4afc25305eb
Vlad <kristoyt@bk.ru>
2022-03-25 00:37:38 +0300

    fix test error 10

--- src/renders/renderByFormStatus.js
+++ src/renders/renderByFormStatus.js
@@ -1,20 +1,24 @@
 const renderByFormStatus = {
   ready: (elements) => {
+    elements.$urlInput.readOnly = false;
     elements.$submitBtn.disabled = false;
     elements.$urlInput.classList.remove('is-invalid');
   },
   error: (elements) => {
+    elements.$urlInput.readOnly = false;
     elements.$submitBtn.disabled = false;
     elements.$feedback.classList.add('text-danger');
     elements.$feedback.classList.remove('text-success');
   },
   validationError: (elements) => {
+    elements.$urlInput.readOnly = false;
     elements.$submitBtn.disabled = false;
     elements.$urlInput.classList.add('is-invalid');
     elements.$feedback.classList.add('text-danger');
     elements.$feedback.classList.remove('text-success');
   },
   success: (elements) => {
+    elements.$urlInput.readOnly = false;
     elements.$submitBtn.disabled = false;
     elements.$urlInput.classList.remove('is-invalid');
     elements.$urlInput.value = '';
@@ -24,6 +28,7 @@ const renderByFormStatus = {
   },
   waiting: (elements) => {
     elements.$urlInput.classList.remove('is-invalid');
+    elements.$urlInput.readOnly = true;
     elements.$submitBtn.disabled = true;
   },
 };
