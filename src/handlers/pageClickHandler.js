a32d462ccf096471a99df1ff52c1a44a9e2e4d18
Vlad <kristoyt@bk.ru>
2022-03-25 13:24:37 +0300

    fix test error 18

--- src/handlers/pageClickHandler.js
+++ src/handlers/pageClickHandler.js
@@ -1,18 +1,25 @@
 function pageClickHandler(state, event) {
   switch (event.target.dataset.action) {
     case 'showModal': {
-      const post = state.posts.find((post) => post.id === event.target.dataset.postId);
+    //   const post = state.posts.find((post) => post.id === event.target.dataset.postId);
+    //   post.viewed = true;
       /* eslint-disable no-console */
       console.log(state.posts);
-      console.log(post);
       console.log(event.target.dataset.postId);
       /* eslint-enable no-console */
-      post.viewed = true;
+      let postData;
+      state.posts.forEach((post) => {
+        if (post.id === event.target.dataset.postId) {
+          post.viewed = true;
+          postData = { ...post };
+        }
+      });
+
       state.showModalWithData = {
-        elementId: post.id,
-        title: post.title,
-        description: post.description,
-        url: post.url,
+        elementId: postData.id,
+        title: postData.title,
+        description: postData.description,
+        url: postData.url,
       };
 
       break;
